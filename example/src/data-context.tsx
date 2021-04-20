8df733a03436b5f771cef95610d7fdf0deb83623
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-04-20 06:01:19 +0000

    add context

---
+++ example/src/data-context.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import { ReactDataContext } from '@themost/react'
+export const DataContext = React.createContext(new ReactDataContext('/'));
---
+++ example/src/services/UserService.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {DataContext} from '../data-context';
+
+export declare interface Group {
+   id: number;
+  name: string;
+  alternateName: string;
+  dateCreated?: Date;
+  dateModified?: Date;
+}
+
+export declare interface User {
+  id: number;
+  name: string;
+  alternateName?: string;
+  dateCreated?: Date;
+  dateModified?: Date;
+  groups: Group[];
+  access_token?: string;
+}
+
+export class UserService {
+  getCurrentUser() {
+    const currentUserValue = localStorage.getItem('currentUser');
+    let currentUser = null;
+    if (currentUserValue) {
+      currentUser = JSON.stringify(currentUserValue)
+    }
+    return currentUser;
+  }
+
+  async tryCurrentUser(access_token: string): Promise<User | null> {
+    const context = new ReactDataContext('/api/');
+    context.setBearerAuthorization(access_token);
+    // get user
+    const user = await context.model(`Users/Me`).asQueryable().expand('groups').getItem();
+    if (user == null) {
+      return null;
+    }
+    // assign the given access token
+    Object.assign(user, {
+      access_token: access_token
+    });
+    // set user
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    // and return
+    return user as User;
+  }
+
+  redirectToLogin() {
+    window.location.href = "/auth/login?client_id=9165351833584149&scope=profile&redirect_uri=/";
+    return (
+      <></>
+    )
+  }
+
+}
